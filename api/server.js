3a2e6b5ec839b4574972865e04ace6164a9cd4da
Folo900911 <rupp93@gmail.com>
2025-10-02 12:15:16 +0300

    fix(vercel): export serverless(app) directly to avoid per-invocation wrapper

--- api/server.js
+++ api/server.js
@@ -1,7 +1,4 @@
 const serverless = require('serverless-http');
 const { app } = require('../app');
 
-module.exports = (req, res) => {
-  const handler = serverless(app);
-  return handler(req, res);
-};
+module.exports = serverless(app);
